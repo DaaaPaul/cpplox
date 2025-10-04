27930d6c43a0adeb97c39b667d3ef2be495e8444
DaaaPaul <paulpeng2008@gmail.com>
2025-10-03 21:52:08 -0400

    add generateExprVisitor to map for later

--- tools/main.cpp
+++ tools/main.cpp
@@ -2,9 +2,9 @@
 #include "GenerateAst.hpp"
 
 int main() {
-	GenerateAst::generate(R"(..\cpplox)", "Expr", "", {}, {});
-	GenerateAst::generate(R"(..\cpplox)", "Literal", "Expr", { "std::any~value" }, { "Expr.h", "<any>"});
-	GenerateAst::generate(R"(..\cpplox)", "Grouping", "Expr", { "Expr~expr" }, { "Expr.h" });
-	GenerateAst::generate(R"(..\cpplox)", "Unary", "Expr", { "Token~op", "Expr~right" }, { "Expr.h", "Token.hpp" });
-	GenerateAst::generate(R"(..\cpplox)", "Binary", "Expr", { "Expr~left", "Token~op", "Expr~right" }, { "Expr.h", "Token.hpp" });
+	GenerateAst::generateExpr(R"(..\cpplox)", "Expr", "", {}, {});
+	GenerateAst::generateExpr(R"(..\cpplox)", "Literal", "Expr", { "std::any~value" }, { "Expr.h", "<any>"});
+	GenerateAst::generateExpr(R"(..\cpplox)", "Grouping", "Expr", { "Expr~expr" }, { "Expr.h" });
+	GenerateAst::generateExpr(R"(..\cpplox)", "Unary", "Expr", { "Token~op", "Expr~right" }, { "Expr.h", "Token.hpp" });
+	GenerateAst::generateExpr(R"(..\cpplox)", "Binary", "Expr", { "Expr~left", "Token~op", "Expr~right" }, { "Expr.h", "Token.hpp" });
 }
