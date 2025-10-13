8f360ca846161d4bfb18dfbbaf066fe4625a2ffc
DaaaPaul <paulpeng2008@gmail.com>
2025-10-13 15:41:33 -0400

    Switch to using compiler-generated constructors for AST classes

--- cpplox/Expr.h
+++ cpplox/Expr.h
@@ -1,4 +1,4 @@
-	#pragma once
+#pragma once
 
 #include <variant>
 #include <memory>
@@ -10,43 +10,25 @@ struct Expr;
 
 struct Grouping {
 	std::unique_ptr<Expr> expression;
-
-	Grouping(Grouping const& g) : expression(std::make_unique<Expr>(*g.expression)) {}
-	Grouping(std::unique_ptr<Expr> expression) : expression(std::move(expression)) {}
 };
 
 struct Binary {
 	std::unique_ptr<Expr> left;
 	Token op;
 	std::unique_ptr<Expr> right;
-
-	Binary(Binary const& b) : left(std::make_unique<Expr>(*b.left)), op(b.op), right(std::make_unique<Expr>(*b.right)) {}
-	Binary(std::unique_ptr<Expr> left, Token op, std::unique_ptr<Expr> right) : left(std::move(left)), op(op), right(std::move(right)) {}
 };
 
 struct Unary {
 	Token op;
 	std::unique_ptr<Expr> right;
-
-	Unary(Unary const& u) : op(u.op), right(std::make_unique<Expr>(*u.right)) {}
-	Unary(Token op, std::unique_ptr<Expr> right) : op(op), right(std::move(right)) {}
 };
 
 struct Literal {
 	std::any value;
-
-	Literal(std::any value) : value(value) {}
 };
 
 struct Expr {
 	std::variant<Grouping, Binary, Unary, Literal> variant;
-
-	Expr(Expr const& e) : variant(e.variant) {}
-	Expr(Expr&& e) noexcept : variant(std::move(e.variant)) {}
-	Expr(Grouping const& g) : variant(g) {}
-	Expr(Binary const& b) : variant(b) {}
-	Expr(Unary const& u) : variant(u) {}
-	Expr(Literal const& l) : variant(l) {}
 
 	template<class R>
 	R accept(const Visitor<R>& visitor) const {
--- cpplox/main.cpp
+++ cpplox/main.cpp
@@ -6,36 +6,31 @@
 #include "AstPrinterVisitor.hpp"
 
 int main() {
-	std::string mode;
-	bool faultyInput = false;
+	//std::string mode;
+	//bool faultyInput = false;
 
-	do {
-		faultyInput = false;
-		std::cout << "Select Mode [p/i]:";
-		std::getline(std::cin, mode);
+	//do {
+	//	faultyInput = false;
+	//	std::cout << "Select Mode [p/i]:";
+	//	std::getline(std::cin, mode);
 
-		if (!(mode == "p" || mode == "i")) {
-			std::cout << "Please select a mode out of: [p for prompt] [i for interpret]\n";
-			faultyInput = true;
-		}
-	} while (faultyInput);
+	//	if (!(mode == "p" || mode == "i")) {
+	//		std::cout << "Please select a mode out of: [p for prompt] [i for interpret]\n";
+	//		faultyInput = true;
+	//	}
+	//} while (faultyInput);
 
-	if (mode == "p") Lox::runPrompt();
-	else if(mode == "i") {
-		std::string filepath;
-		std::cout << "Enter filepath relative to project root directory:";
-		std::getline(std::cin, filepath);
-		Lox::runFile(filepath);
-	}
+	//if (mode == "p") Lox::runPrompt();
+	//else if(mode == "i") {
+	//	std::string filepath;
+	//	std::cout << "Enter filepath relative to project root directory:";
+	//	std::getline(std::cin, filepath);
+	//	Lox::runFile(filepath);
+	//}
 
-	const Expr rightHandNumber(Literal(45.67));
-	const Expr rightHandNumberGrouping(Grouping(std::make_unique<Expr>(rightHandNumber)));
-
-	const Expr leftHandNumber(Literal(123));
-	const Expr unaryLeftHandNumber(Unary(Token(-1, {}, "-", TokenType::MINUS), std::make_unique<Expr>(leftHandNumber)));
-
-	const Expr binary(Binary(std::make_unique<Expr>(unaryLeftHandNumber), Token(-1, {}, "*", TokenType::STAR), std::make_unique<Expr>(rightHandNumberGrouping)));
-
+	Expr rightHandNumberGrouping(Grouping(std::make_unique<Expr>(Literal(45.67))));
+	Expr unaryLeftHandNumber(Unary(Token(-1, {}, "-", TokenType::MINUS), std::make_unique<Expr>(Literal(123))));
+	Expr binary(Binary(std::make_unique<Expr>(std::move(unaryLeftHandNumber)), Token(-1, {}, "*", TokenType::STAR), std::make_unique<Expr>(std::move(rightHandNumberGrouping))));
 	std::cout << AstPrinterVisitor().print(binary);
 
 	return 0;
